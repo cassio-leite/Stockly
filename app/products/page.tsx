9b854d59676efb71c8e1aacf8d7415cb87ddf1f1
Cassio Leite <cassiocarmo77@gmail.com>
2026-02-11 14:36:53 -0300

    feat: add initial product dialog

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,12 @@
-import { PlusIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { DataTable } from "../_components/ui/data-table";
 import { productTableColumns } from "./_components/table-columns";
 import { getProducts } from "../_data-access/product/get-products";
+import AddProductButton from "./_components/add-product-button";
 
 const ProductsPage = async () => {
   const products = await getProducts();
   return (
-    <div className="m-8 w-full space-y-8 bg-white p-8 rounded-lg shadow">
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8 shadow">
       <div className="flex w-full items-start justify-between">
         <div className="space-y-1">
           <span className="text-xs font-semibold text-slate-500">
@@ -15,12 +14,12 @@ const ProductsPage = async () => {
           </span>
           <h2 className="text-xl font-semibold">Produtos</h2>
         </div>
-        <Button className="gap-2">
-          <PlusIcon size={20} />
-          Novo Produto
-        </Button>
+        <AddProductButton />
       </div>
-      <DataTable columns={productTableColumns} data={products} />
+      <DataTable
+        columns={productTableColumns}
+        data={JSON.parse(JSON.stringify(products))}
+      />
     </div>
   );
 };
