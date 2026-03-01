9a964f9f345e0144bcde31335df366fb3d1afb2a
Cassio Leite <cassiocarmo77@gmail.com>
2026-03-01 17:29:45 -0300

    feat: ensure product stock can be zero

--- app/_actions/product/upsert-product/schema.ts
+++ app/_actions/product/upsert-product/schema.ts
@@ -8,15 +8,9 @@ export const upsertProductSchema = z.object({
   price: z.number().min(0.01, {
     message: "O preço do produto é obrigatório.",
   }),
-  stock: z.coerce
-    .number()
-    .positive({
-      message: "O estoque do produto deve ser um número positivo.",
-    })
-    .int()
-    .min(0, {
-      message: "O estoque do produto é obrigatório.",
-    }),
+  stock: z.coerce.number().int().min(0, {
+    message: "O estoque do produto é obrigatório.",
+  }),
 });
 
 export type UpsertProductSchema = z.infer<typeof upsertProductSchema>;
