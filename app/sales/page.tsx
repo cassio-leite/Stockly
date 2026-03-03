83e47c3281c824b4c993f10ba21cce887d892c61
Cassio Leite <cassiocarmo77@gmail.com>
2026-03-03 15:50:44 -0300

    feat: ensure sale can be updated

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -2,7 +2,7 @@ import { ComboboxOption } from "../_components/ui/combobox";
 import { DataTable } from "../_components/ui/data-table";
 import { getProducts } from "../_data-access/product/get-products";
 import { getSales } from "../_data-access/sale/get-sales";
-import CreateSaleButton from "./_components/create-sale-button";
+import UpsertSaleButton from "./_components/create-sale-button";
 import { saleTableColumns } from "./_components/table-columns";
 
 const SalesPage = async () => {
@@ -12,8 +12,13 @@ const SalesPage = async () => {
     value: product.id,
     label: product.name,
   }));
-    return ( 
-        <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8 shadow">
+  const tableData = sales.map((sale) => ({
+    ...sale,
+    products,
+    productOptions,
+  }));
+  return (
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8 shadow">
       <div className="flex w-full items-start justify-between">
         <div className="space-y-1">
           <span className="text-xs font-semibold text-slate-500">
@@ -21,14 +26,11 @@ const SalesPage = async () => {
           </span>
           <h2 className="text-xl font-semibold">Produtos</h2>
         </div>
-        <CreateSaleButton products={products} productOptions={productOptions} />
+        <UpsertSaleButton products={products} productOptions={productOptions} />
       </div>
-      <DataTable
-        columns={saleTableColumns}
-        data={JSON.parse(JSON.stringify(sales))}
-      />
+      <DataTable columns={saleTableColumns} data={tableData} />
     </div>
-     );
-}
- 
+  );
+};
+
 export default SalesPage;
